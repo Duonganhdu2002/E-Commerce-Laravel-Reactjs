7e964c877818dc9ad9bad5472dca31c1ec89af12
Wydanhdu <Wydanhdu@gmail.com>
2023-12-17 11:58:43 +0700

    Setup lại react

--- react/src/components/GuestLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {Outlet} from 'react-router-dom'
-export default function GuestLayout() {
-  return (
-    <div>
-      <Outlet/>
-    </div>
-  )
-}
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { RouterProvider } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import router from './rounter.jsx'
-import { ContextProvider } from './context/ContextProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ContextProvider>
-      <RouterProvider router={router} />
-    </ContextProvider>
-  </React.StrictMode >
+    <App />
+  </React.StrictMode>,
 )
