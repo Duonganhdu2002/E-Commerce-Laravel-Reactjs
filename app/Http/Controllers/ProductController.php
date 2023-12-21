81c1d4f158a5d96ed2ebcfd8baa046ada9af2893
nav0114 <vunhatanh9597@gmail.com>
2023-12-21 21:46:21 +0700

    update product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -69,10 +69,57 @@ class ProductController extends Controller
     return response()->json($arr, 200);
     }
     
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $product)
     {
-        //
+    $input = $request->all();
+
+    $validator = Validator::make($input, [
+        'name' => 'required',
+        'price' => 'required',
+        
+        
+    ]);
+
+    if ($validator->fails()) {
+        $arr = [
+            'status' => false,
+            'message' => 'Lỗi kiểm tra dữ liệu',
+            'data' => $validator->errors()
+        ];
+        return response()->json($arr, 200);
     }
+
+    $product = Product::find($product);
+
+    if (!$product) {
+        $arr = [
+            'status' => false,
+            'message' => 'Sản phẩm không tồn tại',
+            'data' => null
+        ];
+        return response()->json($arr, 404);
+    }
+
+    $product->name = $input['name'] ?? null;
+    $product->price = $input['price'] ?? null;
+    $product->description = $input['description'] ?? null;
+    $product->color_id = $input['color_id'] ?? null;
+    $product->size_id = $input['size_id'] ?? null;
+    $product->created_by_user_id = $input['created_by_user_id'] ?? null;
+    $product->product_category_id = $input['product_category_id'] ?? null;
+    $product-> stock= $input['stock'] ?? null;
+    $product->discount_id = $input['discount_id'] ?? null;
+    $product->save();
+
+    $arr = [
+        'status' => true,
+        'message' => 'Sản phẩm cập nhật thành công',
+        'data' => $product
+    ];
+
+    return response()->json($arr, 200);
+}
+
     public function destroy(string $id)
     {
         try {
