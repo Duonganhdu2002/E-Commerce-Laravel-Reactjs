b44f0676bb306d2b2df20677f44aab8a1708f83c
Wydanhdu <Wydanhdu@gmail.com>
2023-12-26 17:24:27 +0700

    Add route

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,7 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
+import Login from './views/Login';
+import Register from './views/Register';
+
 export default function App() {
   return (
-    <h1 className="bg-blue-200">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
 }
