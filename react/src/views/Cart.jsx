c3c15a2cd9112088fd9deea4f742b17f99f72f02
duco9t <dung5715@gmail.com>
2023-12-26 17:50:38 +0700

    add giỏ hàng và sửa lại vài thứ

---
+++ react/src/views/Cart.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/public/Header'
+import Navbar from '../components/public/navbar'
+import MenuBar from '../components/public/MenuBar'
+import SubFooter from '../components/public/SubFooter'
+import Footer from '../components/public/Footer'
+
+
+
+export default function Cart() {
+  return (
+    <div>
+    <Header/>
+    <Navbar/>
+    <MenuBar/>
+    <SubFooter/>
+    <Footer/>
+    </div>
+  )
+}
