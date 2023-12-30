d3d782e3e067be002385de13827814dabe086b71
Wydanhdu <Wydanhdu@gmail.com>
2023-12-30 23:21:48 +0700

    Thêm folder icon field

--- routes/api.php
+++ routes/api.php
@@ -127,6 +127,10 @@ Route::prefix('public')->group(function () { // truy xuất dữ liệu ra trang
     Route::resource('Transaction', TransactionController::class);
 
     Route::resource('UserPayment', UserPaymentController::class);
+
+    Route::resource('Field', FieldController::class);
+
+    Route::resource('Brand', BrandController::class);
 });
 
 
