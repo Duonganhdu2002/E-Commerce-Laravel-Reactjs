5a7fb5ac2a6b73f6b46f76d785ab430adca30a90
duco9t <dung5715@gmail.com>
2024-01-06 07:10:40 +0700

    Add layout thông tin trang shop  6/1/2024

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,24 +1,26 @@
 // App.jsx
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Cart from "./pages/Cart";
-import Field from './pages/Field';
-import ProductDetails from './pages/ProductDetails';
+import Field from "./pages/Field";
+import ProductDetails from "./pages/ProductDetails";
+import Shop from "./pages/Shop";
 
 export default function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/fields/:fieldId" element={<Field/>} />
-        <Route path="/productdetails" element={<ProductDetails/>} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/fields/:fieldId" element={<Field />} />
+                <Route path="/productdetails" element={<ProductDetails />} />
+                <Route path="/shop" element={<Shop />} />
+            </Routes>
+        </div>
+    );
 }
