d6d9bbb9a9e03a6c649b255b623f38adb0b3c322
duco9t <dung5715@gmail.com>
2024-01-13 22:57:24 +0700

    Fix lại tất cả giao diện  13/1/2024

--- react/src/components/ui/Container.jsx
+++ react/src/components/ui/Container.jsx
@@ -1,50 +1,43 @@
-import Sale3 from "../../assets/sale/sale3.png";
-import Sale4 from "../../assets/sale/sale4.png";
-import Sale5 from "../../assets/sale/sale5.png";
-import Sale6 from "../../assets/sale/sale6.png";
-import Sale7 from "../../assets/sale/sale7.png";
-import Sale8 from "../../assets/sale/sale8.png";
-import { useState, useEffect } from "react";
+import { Carousel } from "@material-tailwind/react";
+
+import sale3 from "../../assets/sale/sale3.png";
+import sale4 from "../../assets/sale/sale4.png";
+import sale5 from "../../assets/sale/sale5.png";
+import sale6 from "../../assets/sale/sale6.png";
+import sale7 from "../../assets/sale/sale7.png";
+import sale8 from "../../assets/sale/sale8.png";
 
 const Container = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    // Mảng chứa các đường dẫn ảnh
-    const imagePaths = [Sale3, Sale4, Sale5, Sale6, Sale7, Sale8];
-
-    // Thời gian giữa các lần thay đổi ảnh (7 giây)
-    const intervalTime = 7000;
-
-    useEffect(() => {
-        // Hàm thay đổi ảnh
-        const changeImage = () => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % imagePaths.length);
-        };
-
-        // Thiết lập hàm thay đổi ảnh tự động sau mỗi khoảng thời gian
-        const interval = setInterval(changeImage, intervalTime);
-
-        // Xóa interval khi component unmount
-        return () => clearInterval(interval);
-    }, [currentIndex, imagePaths.length]);
+    const imagePaths = [sale3, sale4, sale5, sale6, sale7, sale8];
 
     return (
-        <div className=" flex items-center justify-center ">
-            <div
-                className="2xl:w-[80%] xl:w-[80%] lg:w-[80%] md:w-[90%] w-[95%]"
+        <div className="flex items-center justify-center">
+            <Carousel
+                className="rounded-xl 2xl:w-[80%] xl:w-[80%] lg:w-[80%] md:w-[90%] w-[95%]"
+                navigation={({ setActiveIndex, activeIndex, length }) => (
+                    <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+                        {new Array(length).fill("").map((_, i) => (
+                            <span
+                                key={i}
+                                className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                                    activeIndex === i
+                                        ? "w-8 bg-white"
+                                        : "w-4 bg-white/50"
+                                }`}
+                                onClick={() => setActiveIndex(i)}
+                            />
+                        ))}
+                    </div>
+                )}
+                autoplay={true}
+                loop={true}
             >
-                <img
-                    src={imagePaths[currentIndex]}
-                    alt=""
-                    className="transition-all duration-500 ease-in-out animate-slideLeft cursor-pointer rounded-xl"
-                    onClick={() =>
-                        setCurrentIndex(
-                            (prevIndex) => (prevIndex + 1) % imagePaths.length
-                        )
-                    }
-                />
-            </div>
+                {imagePaths.map((path, index) => (
+                    <img key={index} src={path} alt="" className="rounded-xl" />
+                ))}
+            </Carousel>
         </div>
     );
 };
+
 export default Container;
