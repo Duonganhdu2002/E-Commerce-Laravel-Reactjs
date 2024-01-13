e7ba0ceaa69b86b5db27552069b24208cde0e1cc
Wydanhdu <Wydanhdu@gmail.com>
2024-01-13 22:36:30 +0700

    Duud

--- react/src/components/ui/FurniturePrview.jsx
+++ react/src/components/ui/FurniturePrview.jsx
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Banner4 from "../../assets/banner/banner_4.png";
 import Banner5 from "../../assets/banner/banner_5.png";
-import Cart from "../../assets/icon/add-to-cart.svg"
+import Cart from "../../assets/icon/add-to-cart.svg";
 import { fetchTop6CategoryById } from "../../services/productService";
 
 export default function FurniturePrview() {
-
     const [listTop6Furniture, setListTop6Furniture] = useState([]);
 
     useEffect(() => {
@@ -22,22 +21,20 @@ export default function FurniturePrview() {
         } catch (error) {
             console.error("Error: ", error);
         }
-    }
+    };
 
-    // console.log(listTop6Furniture);
+    // console.log(listTop6Furniture)
 
     return (
         <div className="flex justify-center items-center">
-
             <div className="h-auto w-[80%] mt-8 mb-8">
-
                 <div className="my-auto justify-between items-center grid grid-cols-1 md:grid-cols-2 justify-items-center gap-y-8 gap-x-6 ">
-
                     <div className="h-[200px] w-full relative text-center flex">
                         <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-base">
                             <p>NEW ARRIVALS</p>
                             <p>
-                                <span className=" font-bold text-2xl">Bedroom</span> <span>Sets</span>
+                                <span className=" font-bold text-2xl">Bedroom</span>{" "}
+                                <span>Sets</span>
                             </p>
                             <p>Temport sem finibus</p>
                             <p className=" font-bold">$299.00</p>
@@ -52,20 +49,18 @@ export default function FurniturePrview() {
                         />
                     </div>
 
-
-
                     <div className="h-[200px] w-full relative text-center mt-4 ">
                         <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-base">
                             <p>BEST OFFERS</p>
                             <p>
-                                <span className=" font-bold text-2xl">Soft</span> <span>Chairs</span>
+                                <span className=" font-bold text-2xl">Soft</span>{" "}
+                                <span>Chairs</span>
                             </p>
                             <p>Temport sem finibus</p>
                             <p className=" font-bold">$179.00</p>
                             <Link to="#">
                                 <button className=" mt-4 bg-slate-800 bg-opacity-70 p-2 px-4 border rounded-full">See more</button>
                             </Link>
-
                         </div>
                         <img
                             src={Banner5}
@@ -73,39 +68,42 @@ export default function FurniturePrview() {
                             className="w-full h-full object-cover rounded-xl"
                         />
                     </div>
-
                 </div>
 
                 <div className="w-[100%] mt-12">
-
-                    <section id="Projects"
-                        class=" mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-8 gap-x-6 mt-6 mb-5">
-                        {
-                            listTop6Furniture && listTop6Furniture.length > 0 && listTop6Furniture.map((product, index) => (
-                                <div key={index} class="w-full h-120 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-                                    <a href="#">
-                                        <img src={`/src/assets/image/${product.images[1]}`}
-                                            alt="Product" class=" h-[80%] w-full object-cover rounded-t-xl" />
-                                        <div class="px-4 py-3 w-full">
-                                            <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
-                                            <p class="text-lg font-bold text-black truncate block capitalize">{product.name}</p>
-                                            <div class="flex items-center">
-                                                <p class="text-lg font-semibold text-black cursor-auto my-3">${product.price - (product.price) * 0.1}</p>
+                    <section
+                        id="Projects"
+                        className=" mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-8 gap-x-6 mt-6 mb-5"
+                    >
+                        {listTop6Furniture &&
+                            listTop6Furniture.length > 0 &&
+                            listTop6Furniture.map((product, index) => (
+                                <Link key={index} to={`/product/${product.product_id}`}>
+                                    <div className="w-full h-[550px] bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+                                        <img
+                                            src={`/src/assets/image/${product.images[1]}`}
+                                            alt="Product"
+                                            className=" h-[80%] w-full object-cover rounded-t-xl"
+                                        />
+                                        <div className="px-4 py-3 w-full">
+                                            <span className="text-gray-400 mr-3 uppercase text-xs">Brand</span>
+                                            <p className="text-lg font-bold text-black truncate block capitalize">{product.name}</p>
+                                            <div className="flex items-center">
+                                                <p className="text-lg font-semibold text-black cursor-auto my-3">${product.price - (product.price) * 0.1}</p>
                                                 <del>
-                                                    <p class="text-sm text-gray-600 cursor-auto ml-2">${product.price}</p>
+                                                    <p className="text-sm text-gray-600 cursor-auto ml-2">${product.price}</p>
                                                 </del>
-                                                <div class="ml-auto">
+                                                <div className="ml-auto">
                                                     <img src={Cart} alt="" />
                                                 </div>
                                             </div>
                                         </div>
-                                    </a>
-                                </div>
-                            ))
-                        }
+                                    </div>
+                                </Link>
+
+                            ))}
                     </section>
                 </div>
-
             </div>
         </div>
     );
--- react/src/components/ui/SmartPhone.jsx
+++ react/src/components/ui/SmartPhone.jsx
@@ -1,16 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import Banner6 from '../../assets/banner/banner_6.png'
-import Banner7 from '../../assets/banner/banner_7.png'
-import { fetchTop6CategoryById } from '../../services/productService'
-import Cart from "../../assets/icon/add-to-cart.svg"
-
-
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Banner6 from '../../assets/banner/banner_6.png';
+import Banner7 from '../../assets/banner/banner_7.png';
+import { fetchTop6CategoryById } from '../../services/productService';
+import Cart from "../../assets/icon/add-to-cart.svg";
 
 export default function SmartPhone() {
-
     const [listTop6SmartPhone, setListTop6SmartPhone] = useState([]);
-
 
     useEffect(() => {
         getFetchTop6CategoryById();
@@ -27,20 +23,16 @@ export default function SmartPhone() {
         }
     }
 
-    // console.log(listTop6SmartPhone);
-
     return (
         <div className="flex justify-center items-center">
-
             <div className="h-auto w-[80%] mt-8 mb-8">
-
                 <div className="my-auto justify-between items-center grid grid-cols-1 md:grid-cols-2 justify-items-center gap-y-8 gap-x-6 ">
-
                     <div className="h-[200px] w-full relative text-center flex">
                         <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-base">
                             <p>NEW ARRIVALS</p>
                             <p>
-                                <span className=" font-bold text-2xl">Bedroom</span> <span>Sets</span>
+                                <span className=" font-bold text-2xl">Bedroom</span>{" "}
+                                <span>Sets</span>
                             </p>
                             <p>Temport sem finibus</p>
                             <p className=" font-bold">$299.00</p>
@@ -55,20 +47,18 @@ export default function SmartPhone() {
                         />
                     </div>
 
-
-
                     <div className="h-[200px] w-full relative text-center mt-4 ">
                         <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-base">
                             <p>BEST OFFERS</p>
                             <p>
-                                <span className=" font-bold text-2xl">Soft</span> <span>Chairs</span>
+                                <span className=" font-bold text-2xl">Soft</span>{" "}
+                                <span>Chairs</span>
                             </p>
                             <p>Temport sem finibus</p>
                             <p className=" font-bold">$179.00</p>
                             <Link to="#">
                                 <button className=" mt-4 bg-slate-800 bg-opacity-70 p-2 px-4 border rounded-full">See more</button>
                             </Link>
-
                         </div>
                         <img
                             src={Banner7}
@@ -76,43 +66,43 @@ export default function SmartPhone() {
                             className="w-full h-full object-cover rounded-xl"
                         />
                     </div>
-
                 </div>
 
                 <div className="w-[100%] mt-12">
-
-                    <section id="Projects"
-                        class=" mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-8 gap-x-6 mt-6 mb-5">
-                        {
-                            listTop6SmartPhone && listTop6SmartPhone.length > 0 && listTop6SmartPhone.map((product, index) => (
-                                <div key={index} class="w-full h-120 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-                                    <a href="#">
-                                        <img src={`/src/assets/image/${product.images[0]}`}
-                                            alt="Product" class=" h-[80%] w-full object-cover rounded-t-xl" />
-                                        <div class="px-4 py-3 w-full">
-                                            <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
-                                            <p class="text-lg font-bold text-black truncate block capitalize">{product.name}</p>
-                                            <div class="flex items-center">
-                                                <p class="text-lg font-semibold text-black cursor-auto my-3">${product.price - (product.price) * 0.1}</p>
+                    <section
+                        id="Projects"
+                        className=" mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-8 gap-x-6 mt-6 mb-5"
+                    >
+                        {listTop6SmartPhone &&
+                            listTop6SmartPhone.length > 0 &&
+                            listTop6SmartPhone.map((product, index) => (
+                                <Link key={index} to={`/product/${product.product_id}`}>
+                                    <div className="w-full h-[550px] bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+                                        <img
+                                            src={`/src/assets/image/${product.images[1]}`}
+                                            alt="Product"
+                                            className=" h-[80%] w-full object-cover rounded-t-xl"
+                                        />
+                                        <div className="px-4 py-3 w-full">
+                                            <span className="text-gray-400 mr-3 uppercase text-xs">Brand</span>
+                                            <p className="text-lg font-bold text-black truncate block capitalize">{product.name}</p>
+                                            <div className="flex items-center">
+                                                <p className="text-lg font-semibold text-black cursor-auto my-3">${product.price - (product.price) * 0.1}</p>
                                                 <del>
-                                                    <p class="text-sm text-gray-600 cursor-auto ml-2">${product.price}</p>
+                                                    <p className="text-sm text-gray-600 cursor-auto ml-2">${product.price}</p>
                                                 </del>
-                                                <div class="ml-auto">
+                                                <div className="ml-auto">
                                                     <img src={Cart} alt="" />
                                                 </div>
                                             </div>
                                         </div>
-                                    </a>
-                                </div>
-                            ))
-                        }
+                                    </div>
+                                </Link>
+
+                            ))}
                     </section>
                 </div>
-
-
-
-
             </div>
         </div>
-    )
+    );
 }
