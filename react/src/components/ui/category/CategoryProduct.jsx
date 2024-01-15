53b948275a6a2fb49be8a750788900f450b1d973
Wydanhdu <Wydanhdu@gmail.com>
2024-01-15 19:52:15 +0700

    Du

--- react/src/components/ui/category/CategoryProduct.jsx
+++ react/src/components/ui/category/CategoryProduct.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { fetchProductWithCategory } from "../../../services/productService";
+import Cart from "../../../assets/icon/add-to-cart.svg";
 
 export default function CategoryProduct() {
     const { categoryId } = useParams();
@@ -27,74 +28,75 @@ export default function CategoryProduct() {
     return (
         <div className="flex justify-center items-center py-5">
             <div className="w-[95%] md:w-[90%] lg:w-[80%]">
-                {data &&
-                    data.length > 0 &&
-                    data.map((product, index) => (
-                        <Link key={index} to={`/product/${product.product_id}`}>
-                            <section className="p-2 transform duration-500 w-1/2 md:w-1/3 xl:w-1/4 2xl:w-1/5 float-left cursor-pointer">
-                                <div className="flex flex-col items-center bg-[#475877]/10 rounded-xl text-center py-6 px-4 mt-4">
-                                    <div className=" flex justify-center transform duration-500 hover:-translate-y-2">
-                                        <img
-                                            className=" w-[250px] h-[250px] rounded-xl object-cover"
-                                            src={`../../../../src/assets/image/${product.images[0]}`}
-                                            alt=""
-                                        />
+                <section
+                    id="Projects"
+                    className=" mx-auto grid grid-cols-1 lg:grid-cols-3  md:grid-cols-2 2xl:grid-cols-4 justify-items-center justify-center gap-y-8 gap-x-6 mt-6 mb-5"
+                >
+                    {data &&
+                        data.length > 0 &&
+                        data.map((product, index) => (
+                            <Link key={index} to={`/product/${product.product_id}`}>
+                                <div className="w-full h-[600px] bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+                                    <img
+                                        className=" h-[70%] w-[340px] object-cover rounded-t-xl"
+                                        src={`../../../../src/assets/image/${product.images[0]}`}
+                                        alt="Product"
+                                    />
+                                    <div className="px-4 py-3 h-[20%] w-full">
+                                        <span className="text-gray-400 mr-3 uppercase text-xs">Brand</span>
+                                        <p className="text-lg font-bold text-black truncate block capitalize">{product.name}</p>
+                                        <div className="space-x-1 flex justify-center mt-10">
+                                            <svg
+                                                className="w-4 h-4 mx-px fill-current text-orange-600"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 14 14"
+                                            >
+                                                <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
+                                            </svg>
+                                            <svg
+                                                className="w-4 h-4 mx-px fill-current text-orange-600"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 14 14"
+                                            >
+                                                <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
+                                            </svg>
+                                            <svg
+                                                className="w-4 h-4 mx-px fill-current text-orange-600"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 14 14"
+                                            >
+                                                <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
+                                            </svg>
+                                            <svg
+                                                className="w-4 h-4 mx-px fill-current text-orange-600"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 14 14"
+                                            >
+                                                <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
+                                            </svg>
+                                            <svg
+                                                className="w-4 h-4 mx-px fill-current text-gray-300"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 14 14"
+                                            >
+                                                <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
+                                            </svg>
+                                        </div>
+                                        <div className="flex items-center">
+                                            <p className="text-lg font-semibold text-black cursor-auto my-3">${(product.price - (product.price) * 0.3).toFixed(2)}</p>
+                                            <del>
+                                                <p className="text-sm text-gray-600 cursor-auto ml-2">${product.price}</p>
+                                            </del>
+                                            <div className="ml-auto">
+                                                <img src={Cart} alt="" />
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className="space-x-1 flex justify-center mt-10">
-                                        <svg
-                                            className="w-4 h-4 mx-px fill-current text-orange-600"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 14 14"
-                                        >
-                                            <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
-                                        </svg>
-                                        <svg
-                                            className="w-4 h-4 mx-px fill-current text-orange-600"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 14 14"
-                                        >
-                                            <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
-                                        </svg>
-                                        <svg
-                                            className="w-4 h-4 mx-px fill-current text-orange-600"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 14 14"
-                                        >
-                                            <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
-                                        </svg>
-                                        <svg
-                                            className="w-4 h-4 mx-px fill-current text-orange-600"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 14 14"
-                                        >
-                                            <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
-                                        </svg>
-                                        <svg
-                                            className="w-4 h-4 mx-px fill-current text-gray-300"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 14 14"
-                                        >
-                                            <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
-                                        </svg>
-                                    </div>
-                                    <h1 className="text-md my-5">
-                                        {product.name}
-                                    </h1>
-                                    <h2 className=" text-xl font-semibold mb-2">
-                                        ${product.price}
-                                    </h2>
-                                    {/* <div className=" flex flex-wrap mt-auto ml-auto">
-                                <button className="p-1 px-4 bg-purple-500 text-white rounded-md hover:bg-purple-600">
-                                    Add To Cart
-                                </button>
-                                <button className="p-1 px-4 bg-purple-500 text-white rounded-md hover:bg-purple-600">
-                                    Buy now
-                                </button>
-                            </div> */}
                                 </div>
-                            </section>
-                        </Link>
-                    ))}
+                            </Link>
+
+                        ))}
+                </section>
             </div>
         </div>
     );
