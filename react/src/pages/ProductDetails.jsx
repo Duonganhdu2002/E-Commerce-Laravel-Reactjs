99114ad71195a180f22b7e2046570ca09bd40d6d
Wydanhdu <Wydanhdu@gmail.com>
2024-01-29 19:12:47 +0700

    fix

--- react/src/pages/ProductDetails.jsx
+++ react/src/pages/ProductDetails.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import LayoutProductDetails from "../components/ui/LayoutProductDetails";
-import LayoutShopInformation from "../components/ui/LayoutShopInformation";
-import ProductPreview from "../components/ui/ProductPreview";
-import ShopAccount from "../components/ui/ShopAccount";
 
 export default function ProductDetails() {
     return (
         <div>
             <LayoutProductDetails />
-            {/* <ShopAccount/> */}
-            {/* <ProductPreview /> */}
         </div>
     );
 }
