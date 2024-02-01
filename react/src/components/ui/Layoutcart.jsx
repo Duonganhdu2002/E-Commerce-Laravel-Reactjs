f0e8951ea0784ae313b53fdebc6d554ec22ff2eb
Wydanhdu <Wydanhdu@gmail.com>
2024-02-01 23:23:47 +0700

    Updat

--- react/src/components/ui/Layoutcart.jsx
+++ react/src/components/ui/Layoutcart.jsx
@@ -57,28 +57,28 @@ const Layoutcart = () => {
                                 <Link key={index} to={`/product/${carts.product_id}`}>
                                     <div className="mt-4 md:mt-6 flex  flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full ">
                                         <div className="pb-4 md:pb-8 w-full md:w-40">
-                                            <img className="w-full hidden md:block" src="https://i.ibb.co/84qQR4p/Rectangle-10.png" alt="dress" />
-                                            <img className="w-full md:hidden" src="https://i.ibb.co/L039qbN/Rectangle-10.png" alt="dress" />
+                                            <img className="w-full hidden md:block" src={`https://github.com/Duonganhdu2002/E-Commerce/blob/main/react/src/assets/image/${carts.img}?raw=true`} alt="dress" />
+                                            <img className="w-full md:hidden" src={`https://github.com/Duonganhdu2002/E-Commerce/blob/main/react/src/assets/image/${carts.img}?raw=true`} alt="dress" />
                                         </div>
                                         <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full  pb-8 space-y-4 md:space-y-0">
                                             <div className="w-full flex flex-col justify-start items-start space-y-8">
-                                                <h3 className="text-xl xl:text-2xl font-semibold leading-6 text-gray-800">Premium Quaility Dress</h3>
+                                                <h3 className="text-xl xl:text-2xl font-semibold leading-6 text-gray-800">{carts.name}</h3>
                                                 <div className="flex justify-start items-start flex-col space-y-2">
 
                                                     <p className="text-sm leading-none text-gray-800">
-                                                        <span className="text-gray-300">Size: </span> Small
+                                                        <span className="text-gray-300">Size: </span> {carts.size}
                                                     </p>
                                                     <p className="text-sm leading-none text-gray-800">
-                                                        <span className="text-gray-300">Color: </span> Light Blue
+                                                        <span className="text-gray-300">Color: </span> {carts.color}
                                                     </p>
                                                 </div>
                                             </div>
                                             <div className="flex justify-between space-x-8 items-start w-full">
                                                 <p className="text-base xl:text-lg leading-6">
-                                                    $36.00
+                                                    ${carts.price}
                                                 </p>
-                                                <p className="text-base xl:text-lg leading-6 text-gray-800">01</p>
-                                                <p className="text-base xl:text-lg font-semibold leading-6 text-gray-800">$36.00</p>
+                                                <p className="text-base xl:text-lg leading-6 text-gray-800">{carts.quantity}</p>
+                                                <p className="text-base xl:text-lg font-semibold leading-6 text-gray-800">  ${parseFloat((carts.price * carts.quantity).toFixed(2))}</p>
                                             </div>
                                         </div>
                                     </div>
