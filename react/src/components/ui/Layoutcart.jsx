adef0d33c5a4be72baf88af21a5cd18a23a08c1f
Wydanhdu <Wydanhdu@gmail.com>
2024-02-02 00:52:10 +0700

    Updat

--- react/src/components/ui/Layoutcart.jsx
+++ react/src/components/ui/Layoutcart.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@material-tailwind/react";
+import { Button, Checkbox } from "@material-tailwind/react";
 import Lock from "../../assets/icon/lock-svgrepo-com.svg";
 import Visa from "../../assets/icon/visa-svgrepo-com.svg";
 import AmericanExpress from "../../assets/icon/american-express-logo-svgrepo-com.svg";
@@ -54,84 +54,42 @@ const Layoutcart = () => {
                         <p className="text-lg md:text-xl font-semibold leading-6 xl:leading-5 text-gray-800">List product</p>
                         {
                             data && data.length > 0 && data.map((carts, index) => (
-                                <Link key={index} to={`/product/${carts.product_id}`}>
-                                    <div className="mt-4 md:mt-6 flex  flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full ">
+                                <div key={index} className="mt-4 md:mt-6 flex  flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full ">
+                                    <div className=" w-[5%]">
+                                        <Checkbox defaultChecked={false ? true : false} />
+                                    </div>
+                                    <Link to={`/product/${carts.product_id}`}>
                                         <div className="pb-4 md:pb-8 w-full md:w-40">
                                             <img className="w-full hidden md:block" src={`https://github.com/Duonganhdu2002/E-Commerce/blob/main/react/src/assets/image/${carts.img}?raw=true`} alt="dress" />
                                             <img className="w-full md:hidden" src={`https://github.com/Duonganhdu2002/E-Commerce/blob/main/react/src/assets/image/${carts.img}?raw=true`} alt="dress" />
                                         </div>
-                                        <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full  pb-8 space-y-4 md:space-y-0">
-                                            <div className="w-full flex flex-col justify-start items-start space-y-8">
-                                                <h3 className="text-xl xl:text-2xl font-semibold leading-6 text-gray-800">{carts.name}</h3>
-                                                <div className="flex justify-start items-start flex-col space-y-2">
+                                    </Link>
+                                    <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full  pb-8 space-y-4 md:space-y-0">
+                                        <div className="w-full flex flex-col justify-start items-start space-y-8">
+                                            <h3 className="text-xl xl:text-2xl font-semibold leading-6 text-gray-800">{carts.name}</h3>
+                                            <div className="flex justify-start items-start flex-col space-y-2">
 
-                                                    <p className="text-sm leading-none text-gray-800">
-                                                        <span className="text-gray-300">Size: </span> {carts.size}
-                                                    </p>
-                                                    <p className="text-sm leading-none text-gray-800">
-                                                        <span className="text-gray-300">Color: </span> {carts.color}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                            <div className="flex justify-between space-x-8 items-start w-full">
-                                                <p className="text-base xl:text-lg leading-6">
-                                                    ${carts.price}
+                                                <p className="text-sm leading-none text-gray-800">
+                                                    <span className="text-gray-300">Size: </span> {carts.size}
                                                 </p>
-                                                <p className="text-base xl:text-lg leading-6 text-gray-800">{carts.quantity}</p>
-                                                <p className="text-base xl:text-lg font-semibold leading-6 text-gray-800">  ${parseFloat((carts.price * carts.quantity).toFixed(2))}</p>
+                                                <p className="text-sm leading-none text-gray-800">
+                                                    <span className="text-gray-300">Color: </span> {carts.color}
+                                                </p>
                                             </div>
                                         </div>
+                                        <div className="flex justify-between space-x-8 items-start w-full">
+                                            <p className="text-base xl:text-lg leading-6">
+                                                ${carts.price}
+                                            </p>
+                                            <p className="text-base xl:text-lg leading-6 text-gray-800">{carts.quantity}</p>
+                                            <p className="text-base xl:text-lg font-semibold leading-6 text-gray-800">  ${parseFloat((carts.price * carts.quantity).toFixed(2))}</p>
+                                        </div>
                                     </div>
-                                </Link>
+                                </div>
                             ))
                         }
                     </div>
-                    <div className="flex justify-center md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
-                        <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6   ">
-                            <h3 className="text-xl font-semibold leading-5 text-gray-800">Summary</h3>
-                            <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
-                                <div className="flex justify-between  w-full">
-                                    <p className="text-base leading-4 text-gray-800">Subtotal</p>
-                                    <p className="text-base leading-4 text-gray-600">$56.00</p>
-                                </div>
-                                <div className="flex justify-between items-center w-full">
-                                    <p className="text-base leading-4 text-gray-800">
-                                        Discount <span className="bg-gray-200 p-1 text-xs font-medium leading-3  text-gray-800">STUDENT</span>
-                                    </p>
-                                    <p className="text-base leading-4 text-gray-600">-$28.00 (50%)</p>
-                                </div>
-                                <div className="flex justify-between items-center w-full">
-                                    <p className="text-base leading-4 text-gray-800">Shipping</p>
-                                    <p className="text-base leading-4 text-gray-600">$8.00</p>
-                                </div>
-                            </div>
-                            <div className="flex justify-between items-center w-full">
-                                <p className="text-base font-semibold leading-4 text-gray-800">Total</p>
-                                <p className="text-base font-semibold leading-4 text-gray-600">$36.00</p>
-                            </div>
-                        </div>
-                        <div className="flex flex-col justify-center px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6   ">
-                            <h3 className="text-xl font-semibold leading-5 text-gray-800">Shipping</h3>
-                            <div className="flex justify-between items-start w-full">
-                                <div className="flex justify-center items-center space-x-4">
-                                    <div className="w-8 h-8">
-                                        <img className="w-full h-full" alt="logo" src="https://i.ibb.co/L8KSdNQ/image-3.png" />
-                                    </div>
-                                    <div className="flex flex-col justify-start items-center">
-                                        <p className="text-lg leading-6 font-semibold text-gray-800">
-                                            DPD Delivery
-                                            <br />
-                                            <span className="font-normal">Delivery with 24 Hours</span>
-                                        </p>
-                                    </div>
-                                </div>
-                                <p className="text-lg font-semibold leading-6 text-gray-800">$8.00</p>
-                            </div>
-                            <div className="w-full flex justify-center items-center">
-                                <button className="hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 py-5 w-96 md:w-full bg-gray-800 text-base font-medium leading-4 text-white">View Carrier Details</button>
-                            </div>
-                        </div>
-                    </div>
+
                 </div>
                 <div className=" flex flex-col justify-center w-full xl:w-[40%] h-auto xl:ml-20 ">
                     <div className="rounded-2xl p-8 border">
@@ -143,11 +101,11 @@ const Layoutcart = () => {
                         <div className="">
                             <div className="flex justify-between items-center py-4 border-b text-gray-600">
                                 <p>Subtotal</p>
-                                <p>$2,090</p>
+                                <p>$723</p>
                             </div>
                             <div className="flex justify-between items-center py-4 border-b text-gray-600">
-                                <p>Shipping Tax</p>
-                                <p>$0</p>
+                                <p>Discount</p>
+                                <p>$29</p>
                             </div>
                         </div>
                         <div className="flex justify-between items-center py-4 text-2xl font-bold">
