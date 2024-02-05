1f8731ebf15eb21714aff3b3b26745eef3877ecb
nav0114 <vunhatanh9597@gmail.com>
2024-02-06 00:43:55 +0700

    shop rating 2

--- react/src/components/ui-bussiness/ShopRating.jsx
+++ react/src/components/ui-bussiness/ShopRating.jsx
@@ -82,7 +82,7 @@ export function ShopRating() {
                             selected={selectedDate}
                             onChange={(date) => setSelectedDate(date)}
                             dateFormat="MM/dd/yyyy"
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6 pl-2"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6 pl-2GIT"
                         />
                     </div>
                 </div>
