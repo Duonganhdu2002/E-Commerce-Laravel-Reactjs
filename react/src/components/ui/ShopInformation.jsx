9aa936e2745a4e0a1d163123d37f2b6c42f3e5b8
duco9t <dung5715@gmail.com>
2024-02-23 20:32:15 +0700

    Dũng

---
+++ react/src/components/ui/ShopInformation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function ShopInformation() {
+    return (
+        <div className=' space-y-2 font-medium'>
+            <p>Shop Name: Shop Hoa</p>
+            <p>Email: hehe@haha.hoho</p>
+            <p>Phone number: 0999999999</p>
+            <p>Products: 99+</p>
+            <p>Total orders sold: 159</p>
+            <p>Star rating: 4.9/5.0</p>
+            <p>
+                Shop Description:
+                Abcdef ghjklm nopq asd ksa aksd sk dasie lallskkd a;sdjr ajsyed ajbshuhd asdkjyd asjdayuadj apsoondk kaljbde dajhd ự djah wdj awjhd awh dawidyvaiy dqewhdh ajsbdaida jbdiue aeh vfeh ak v sdh a dajsh dạ db yavw dqw đuya askjdajb đahạ jh wdhw da hj
+                aopsjpdad asoidaiosd ao dao o sdoia oisda sjjeiej aiosuadjsa adusydows aidusu uajd awidwajdwad adusdaj F3ui 3kheh 3ie3 eh23 h3 hr h2 hr h23r 23 r 32hr23r32hhr i32r h32 rfi23 ri23ri23h rhi23 rh32i i
+            </p>
+        </div>
+    )
+}
