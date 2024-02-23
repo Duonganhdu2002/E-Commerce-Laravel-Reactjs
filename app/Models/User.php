dde473492659c35f1188ba3520d8c7e674432e45
nav0114 <vunhatanh9597@gmail.com>
2024-02-23 20:43:56 +0700

    category user

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,12 +14,6 @@ class User extends Authenticatable implements JWTSubject
     use HasApiTokens, HasFactory, Notifiable;
     protected $table = 'users';
 
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'username',
         'email',
@@ -36,21 +30,13 @@ class User extends Authenticatable implements JWTSubject
 
     protected $primaryKey = 'user_id';
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *  
-     * @var array<int, string>
-     */
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
@@ -70,5 +56,10 @@ class User extends Authenticatable implements JWTSubject
     public function user_address()
     {
         return $this->hasMany(user_address::class, 'user_id', 'user_id');
+    }
+
+    public function products()
+    {
+        return $this->hasMany(product::class, 'created_by_user_id', 'user_id');
     }
 }
