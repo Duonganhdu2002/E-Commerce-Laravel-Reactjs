d8291d25bd2df50112b75501bab515cc651c26d6
duco9t <dung5715@gmail.com>
2024-02-28 18:51:04 +0700

    Add Layout Này Nọ Kia

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -23,7 +23,6 @@ import LoginAdmin from "./components/ui-admin/Login";
 import RegisterAdmin from "./components/ui-admin/Register";
 import ProfileAdmin from "./components/ui-admin/Profile";
 import InboxAdmin from "./components/ui-admin/Inbox";
-import MyProductsAdmin from "./pages/admin/MyProducts";
 import DashboardAdmin from "./pages/admin/Dashboard";
 import TaskBarAdmin from "./components/ui-admin/TaskBar";
 import Home from "./pages/Home";
@@ -53,6 +52,14 @@ import FieldList from "./components/ui-admin/FieldList";
 import CategoryList from "./components/ui-admin/CategoryList";
 import BrandList from "./components/ui-admin/BrandList";
 import OrderList from "./components/ui-admin/OrderList";
+import OrderUser from "./components/ui-admin/OrderUser";
+import OrderStatusList from "./components/ui-admin/OrderStatus";
+import ProductList from "./components/ui-admin/ProductList";
+import ProductUserList from "./components/ui-admin/ProductUser";
+import ReviewList from "./components/ui-admin/ReviewList";
+import ReviewUserList from "./components/ui-admin/ReviewUser";
+import ShoppingMethodList from "./components/ui-admin/ShoppingMethod";
+
 
 const Customer = () => {
     return (
@@ -214,14 +221,13 @@ const App = () => {
                                 <Route path="category-list" element={<PrivateAdminRoute element={<CategoryList />} />} />
                                 <Route path="brand-list" element={<PrivateAdminRoute element={<BrandList />} />} />
                                 <Route path="order-list" element={<PrivateAdminRoute element={<OrderList />} />} />
-                                <Route path="order-user" element={<PrivateAdminRoute element={<div>Cancelation</div>} />} />
-                                <Route path="my-products" element={<PrivateAdminRoute element={<MyProductsAdmin />} />} />
-                                <Route path="order-status-list" element={<PrivateAdminRoute element={<div>Add New Product</div>} />} />
-                                <Route path="product-list" element={<PrivateAdminRoute element={<div>Cancelation</div>} />} />
-                                <Route path="product-user-list" element={<PrivateAdminRoute element={<div>Cancelation</div>} />} />
-                                <Route path="review-list" element={<PrivateAdminRoute element={<div>Cancelation</div>} />} />
-                                <Route path="review-user-list" element={<PrivateAdminRoute element={<div>Cancelation</div>} />} />
-                                <Route path="shopping-method-list" element={<PrivateAdminRoute element={<div>Cancelation</div>} />} />
+                                <Route path="order-user" element={<PrivateAdminRoute element={<OrderUser />} />} />
+                                <Route path="order-status-list" element={<PrivateAdminRoute element={<OrderStatusList />} />} />
+                                <Route path="product-list" element={<PrivateAdminRoute element={<ProductList />} />} />
+                                <Route path="product-user-list" element={<PrivateAdminRoute element={<ProductUserList />} />} />
+                                <Route path="review-list" element={<PrivateAdminRoute element={<ReviewList />} />} />
+                                <Route path="review-user-list" element={<PrivateAdminRoute element={<ReviewUserList />} />} />
+                                <Route path="shopping-method-list" element={<PrivateAdminRoute element={<ShoppingMethodList />} />} />
                                 <Route path="dashboard" element={<PrivateAdminRoute element={<DashboardAdmin />} />} />
                                 <Route path="profile" element={<PrivateAdminRoute element={<ProfileAdmin />} />} />
                                 <Route path="settings" element={<PrivateAdminRoute element={<div>Cancelation</div>} />} />
