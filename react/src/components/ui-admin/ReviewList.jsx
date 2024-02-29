35a17a279ebbe752a7f43cec5755bfbc20864c8c
duco9t <dung5715@gmail.com>
2024-02-29 14:09:51 +0700

    Add Layout Hết Phần Admin

--- react/src/components/ui-admin/ReviewList.jsx
+++ react/src/components/ui-admin/ReviewList.jsx
@@ -1,7 +1,194 @@
-import React from 'react'
+import {
+    MagnifyingGlassIcon,
+    ChevronUpDownIcon,
+} from "@heroicons/react/24/outline";
+import { EyeIcon } from "@heroicons/react/24/solid";
+import {
+    Card,
+    CardHeader,
+    Input,
+    Typography,
+    Button,
+    CardBody,
+    CardFooter,
+    Avatar,
+    IconButton,
+    Tooltip,
+} from "@material-tailwind/react";
+
+const TABLE_HEAD = ["ID", "Product Name", "Rating", "Comment", "Create at", "Details"];
+
+const TABLE_ROWS = [
+    {
+        id: "1",
+        product_name: "Smartphones 1d",
+        rating: "5.0/5.0",
+        comment: "Beautiful",
+        date: "23/04/18",
+    },
+    {
+        id: "2",
+        product_name: "Smartphones 1d",
+        rating: "5.0/5.0",
+        comment: "Impressive product, exceeded expectations.",
+        date: "23/04/18",
+    },
+    {
+        id: "3",
+        product_name: "Smartphones 1d",
+        rating: "5.0/5.0",
+        comment: "Beautiful",
+        date: "23/04/18",
+    },
+    {
+        id: "4",
+        product_name: "Smartphones 1d",
+        rating: "5.0/5.0",
+        comment: "Amazing product!",
+        date: "23/04/18",
+    },
+    {
+        id: "5",
+        product_name: "Smartphones 1d",
+        rating: "5.0/5.0",
+        comment: "Good quality, but delivery was slow.",
+        date: "23/04/18",
+    },
+];
 
 export default function ReviewList() {
-  return (
-    <div>ReviewList</div>
-  )
+    return (
+        <Card className="w-full">
+            <CardHeader floated={false} shadow={false} className="rounded-none">
+                <div className="mb-8 flex items-center justify-between gap-8">
+                    <div>
+                        <Typography variant="h5" color="blue-gray">
+                            Review list
+                        </Typography>
+                        <Typography color="gray" className="mt-1 font-normal">
+                            See all review
+                        </Typography>
+                    </div>
+                    <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+                        <Button variant="outlined" size="sm">
+                            view all
+                        </Button>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+                    <div className="w-full md:w-72">
+                        <Input
+                            label="Search"
+                            icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+                        />
+                    </div>
+                </div>
+            </CardHeader>
+            <CardBody className="overflow-scroll px-0">
+                <table className="mt-4 w-full min-w-max table-auto text-left">
+                    <thead>
+                        <tr>
+                            {TABLE_HEAD.map((head, index) => (
+                                <th
+                                    key={head}
+                                    className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
+                                >
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                                    >
+                                        {head}{" "}
+                                        {index !== TABLE_HEAD.length - 1 && (
+                                            <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
+                                        )}
+                                    </Typography>
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {TABLE_ROWS.map(
+                            ({ id, product_name, rating, comment, date }, index) => {
+                                const isLast = index === TABLE_ROWS.length - 1;
+                                const classes = isLast
+                                    ? "p-4"
+                                    : "p-4 border-b border-blue-gray-50";
+
+                                return (
+                                    <tr key={id}>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal"
+                                            >
+                                                {id}
+                                            </Typography>
+                                        </td>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal"
+                                            >
+                                                {product_name}
+                                            </Typography>
+                                        </td>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal w-52"
+                                            >
+                                                {rating}
+                                            </Typography>
+                                        </td>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal"
+                                            >
+                                                {comment}
+                                            </Typography>
+                                        </td>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal"
+                                            >
+                                                {date}
+                                            </Typography>
+                                        </td>
+                                        <td className={classes}>
+                                            <Tooltip content="Details">
+                                                <IconButton variant="text">
+                                                    <EyeIcon className="h-4 w-4" />
+                                                </IconButton>
+                                            </Tooltip>
+                                        </td>
+                                    </tr>
+                                );
+                            },
+                        )}
+                    </tbody>
+                </table>
+            </CardBody>
+            <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                    Page 1 of 10
+                </Typography>
+                <div className="flex gap-2">
+                    <Button variant="outlined" size="sm">
+                        Previous
+                    </Button>
+                    <Button variant="outlined" size="sm">
+                        Next
+                    </Button>
+                </div>
+            </CardFooter>
+        </Card>
+    )
 }
