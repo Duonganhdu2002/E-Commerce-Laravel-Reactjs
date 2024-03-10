9403ef8a9408d15f5a548364da534e133ea8e48f
Wydanhdu <Wydanhdu@gmail.com>
2024-03-10 13:23:26 +0700

    ...

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,20 +6,56 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Stripe\Stripe;
 use Stripe\PaymentIntent;
+use Stripe\Exception\ApiErrorException as Error;
 
 class PaymentController extends Controller
 {
     public function createPaymentIntent(Request $request)
     {
-        // Thiết lập khóa Stripe
-        Stripe::setApiKey(env('STRIPE_SECRET'));
+        try {
+            // Retrieve JSON from POST body
+            $jsonStr = $request->getContent();
+            $jsonObj = json_decode($jsonStr);
 
-        // Tạo PaymentIntent
-        $paymentIntent = PaymentIntent::create([
-            'amount' => $request->amount,
-            'currency' => 'usd', 
-        ]);
+            if (!property_exists($jsonObj, 'items')) {
+                throw new \InvalidArgumentException('Missing "items" property in JSON payload');
+            }
 
-        return response()->json(['clientSecret' => $paymentIntent->client_secret]);
+            // Set up Stripe API key
+            Stripe::setApiKey(env('STRIPE_SECRET'));
+
+            // Create a PaymentIntent with amount and currency
+            $paymentIntent = PaymentIntent::create([
+                'amount' => $this->calculateOrderAmount($jsonObj->items),
+                'currency' => 'usd',
+                'automatic_payment_methods' => [
+                    'enabled' => true,
+                ],
+            ]);
+
+            $output = [
+                'clientSecret' => $paymentIntent->client_secret,
+                'amout' => $this->calculateOrderAmount($jsonObj->items)
+            ];
+
+            return response()->json($output);
+        } catch (\InvalidArgumentException $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        } catch (Error $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    private function calculateOrderAmount(array $items): int
+    {
+        $totalAmount = 0;
+
+        foreach ($items as $item) {
+            $itemArray = (array) $item;
+            if (isset($itemArray['id'], $itemArray['price'], $itemArray['quantity'])) {
+                $totalAmount += $itemArray['price'] * $itemArray['quantity'];
+            }
+        }
+        return $totalAmount;
     }
 }
