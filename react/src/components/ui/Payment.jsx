60b1936c1f385175e34fa03eacee9a33a60102c9
duco9t <dung5715@gmail.com>
2024-03-16 13:24:54 +0700

    CHUNG TA CUA TUONG LAI

--- react/src/components/ui/Payment.jsx
+++ react/src/components/ui/Payment.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { useSelector } from "react-redux";
+import { Spinner } from "@material-tailwind/react";
 
 import CheckoutForm from "./CheckoutForm";
 import "../../App.css";
@@ -10,13 +11,14 @@ import { paymentAction } from "../../services/paymentService";
 
 const stripePromise = loadStripe("pk_test_51OrgAxGsQVxhhN2xWaQ6M9xIGpVwC93X9iDRwSMIWxQpwou7NP5MjNn55WtsQu1ia1ChmO46b8I3qsiiNPz6k3qS00KEa4XTlA");
 
+
 export default function Payment() {
 
     const [clientSecret, setClientSecret] = useState("");
     const selectedItems = useSelector(state => state.cart.items);
     console.log(selectedItems)
     const userId = useSelector(state => state.user.user.user_id || '');
-    
+
     const data = {
         "items": selectedItems,
         "buyer_id": userId,
@@ -50,12 +52,39 @@ export default function Payment() {
         appearance,
     };
 
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true); // Bắt đầu loading
+                let res = await paymentAction(data);
+                setClientSecret(res.clientSecret);
+                setLoading(false); // Kết thúc loading
+            } catch (error) {
+                console.error(error);
+                setLoading(false); // Kết thúc loading nếu có lỗi
+            }
+        };
+        fetchData();
+    }, []);
+
+
     return (
-        <div className="App">
-            {clientSecret && (
-                <Elements options={options} stripe={stripePromise}>
-                    <CheckoutForm />
-                </Elements>
+        <div className="flex h-[50vh] items-center">
+            {loading ? (
+                <div className="flex flex-col gap-y-4 items-center w-full">
+                    <div className="flex justify-center"><Spinner className=" h-12 w-12" /></div>
+
+                    <p className="flex justify-center text-xl font-semibold">Loading</p>
+                </div>
+            ) : (
+                clientSecret && (
+                    <Elements options={options} stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
+                )
             )}
         </div>
     );
