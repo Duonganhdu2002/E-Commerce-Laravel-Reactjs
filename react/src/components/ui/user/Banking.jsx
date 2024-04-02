bd86d9ff169181cab04ec94802e0fe395b89cb25
duco9t <dung5715@gmail.com>
2024-04-03 00:58:04 +0700

    Muốn một này nào đó được A+ :<

--- react/src/components/ui/user/Banking.jsx
+++ react/src/components/ui/user/Banking.jsx
@@ -20,29 +20,14 @@ import {
     Input,
 } from "@material-tailwind/react";
 
-const TABLE_HEAD = ["ID", "Order Id" , "Buyer Id", "Total Amount", "Date"];
+const TABLE_HEAD = ["ID", "Order Id" ,  "Total Amount", "Date"];
 
 export default function Banking() {
-
-    const [active, setActive] = React.useState(1);
-
-    const next = () => {
-        if (active === 10) return;
-
-        setActive(active + 1);
-    };
-
-    const prev = () => {
-        if (active === 1) return;
-
-        setActive(active - 1);
-    };
 
     const user_id = useSelector((state) => state.user.user.user_id);
 
     const [data, setData] = useState([]);
     const [dataFull, setDataFull] = useState([]);
-    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         fetchData(); // Lấy danh sách địa chỉ khi component được mount
@@ -128,15 +113,6 @@ export default function Banking() {
                                             color="blue-gray"
                                             className="font-normal"
                                         >
-                                            {item.buyer_id}
-                                        </Typography>
-                                    </td>
-                                    <td className=" p-4">
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal"
-                                        >
                                             ${item.total_amount}
                                         </Typography>
                                     </td>
@@ -154,28 +130,6 @@ export default function Banking() {
                         </tbody>
                     </table>
                 </CardBody>
-                <CardFooter className="flex items-center justify-center border-blue-gray-50 gap-8">
-                    <IconButton
-                        size="sm"
-                        variant="outlined"
-                        onClick={prev}
-                        disabled={active === 1}
-                    >
-                        <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" />
-                    </IconButton>
-                    <Typography color="gray" className="font-normal">
-                        Page <strong className="text-gray-900">{active}</strong> of{" "}
-                        <strong className="text-gray-900">10</strong>
-                    </Typography>
-                    <IconButton
-                        size="sm"
-                        variant="outlined"
-                        onClick={next}
-                        disabled={active === 10}
-                    >
-                        <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
-                    </IconButton>
-                </CardFooter>
             </Card>
 
         </div>
